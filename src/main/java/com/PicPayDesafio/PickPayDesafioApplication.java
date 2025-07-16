fc6df8ec4dca159ce6630ab5fa0cea52fa4f3f8f
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-16 13:13:01 -0300

    feat: initial config

---
+++ src/main/java/com/PicPayDesafio/PickPayDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.PicPayDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickPayDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickPayDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicPayDesafio/PickPayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPayDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickPayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
