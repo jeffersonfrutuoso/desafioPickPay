05f1f985e8346a0117e313494719c9568eb71e47
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-18 14:09:17 -0300

    feat: adiciona metodo de buscar usuario por id

--- src/main/java/com/PicPayDesafio/repository/UserRepository.java
+++ src/main/java/com/PicPayDesafio/repository/UserRepository.java
@@ -3,6 +3,10 @@ import com.PicPayDesafio.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserById(Long id);
 }
