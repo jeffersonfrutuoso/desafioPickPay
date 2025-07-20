21da1e4a9b5f4d7135cb3fddf03c68dbe589e95a
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-20 13:19:49 -0300

    feat: adiciona a exception de notificação

---
+++ src/main/java/com/PicPayDesafio/excetion/ServiceNotificationException.java
@@ -0,0 +1,7 @@
+package com.PicPayDesafio.excetion;
+
+public class ServiceNotificationException extends RuntimeException{
+    public ServiceNotificationException(String message) {
+        super(message);
+    }
+}
