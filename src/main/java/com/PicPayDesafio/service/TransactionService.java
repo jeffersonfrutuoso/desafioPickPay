46cc70fac1cf3320b0e04d4a2512a8ae6000d723
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-20 13:15:56 -0300

    feat: a logica de autorização do mock no transactionservice, e adiciona a logica de transação

--- src/main/java/com/PicPayDesafio/service/TransactionService.java
+++ src/main/java/com/PicPayDesafio/service/TransactionService.java
@@ -1,32 +1,52 @@
 package com.PicPayDesafio.service;
 
+import com.PicPayDesafio.domain.transaction.Transaction;
 import com.PicPayDesafio.domain.user.User;
 import com.PicPayDesafio.dtos.TransactionDTO;
+import com.PicPayDesafio.excetion.UnauthorizedTransactionException;
 import com.PicPayDesafio.repository.TransactionRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.math.BigDecimal;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @Service
 public class TransactionService {
 
     private final UserService userService;
+    private final AutorizacaoService autorizacaoService;
 
     private final TransactionRepository transactionRepository;
 
-    public TransactionService(UserService userService, TransactionRepository transactionRepository) {
+    public TransactionService(UserService userService, AutorizacaoService autorizacaoService, TransactionRepository transactionRepository) {
         this.userService = userService;
+        this.autorizacaoService = autorizacaoService;
         this.transactionRepository = transactionRepository;
     }
 
+    @Transactional
     public void createTransaction(TransactionDTO transactionDTO){
         User enviador = userService.findUserById(transactionDTO.enviadorId());
         User recebedor = userService.findUserById(transactionDTO.recebedorId());
 
         userService.validarTransaction(enviador, transactionDTO.valor());
+
+        if (!autorizacaoService.validarTransferencia()){
+            throw new UnauthorizedTransactionException("Transação não autorizada pelo mock");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setValorDaTransferencia(transactionDTO.valor());
+        newTransaction.setEnviador(enviador);
+        newTransaction.setRecebedor(recebedor);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        enviador.setCarteira(enviador.getCarteira().subtract(transactionDTO.valor()));
+        recebedor.setCarteira(recebedor.getCarteira().add(transactionDTO.valor()));
+
+        transactionRepository.save(newTransaction);
+        userService.saveUser(enviador);
+        userService.saveUser(recebedor);
     }
+
 }
