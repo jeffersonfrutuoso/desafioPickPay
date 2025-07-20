a1f0cd6802e3d1367c3c3d7c17d87d0ec16f4c26
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-20 13:19:10 -0300

    feat: adiciona o service de notificação

---
+++ src/main/java/com/PicPayDesafio/service/NotificationService.java
@@ -0,0 +1,35 @@
+package com.PicPayDesafio.service;
+
+import com.PicPayDesafio.domain.user.User;
+import com.PicPayDesafio.dtos.mockAutorizacao.NotificacaoDTO;
+import com.PicPayDesafio.excetion.ServiceNotificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    private final RestTemplate restTemplate;
+
+    @Value("${notificacao.url}")
+    private String notificacaoUrl;
+
+    public NotificationService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public void enviarNotificacao(User usuario, String mensagem){
+        String email = usuario.getEmail();
+        NotificacaoDTO notificacaoDTO = new NotificacaoDTO(email, mensagem);
+
+        ResponseEntity<String> notificacaoResponse =  restTemplate.postForEntity(notificacaoUrl, notificacaoDTO, String.class);
+
+        System.out.println("Resposta mock: " + notificacaoResponse.getBody());
+
+        if(!(notificacaoResponse.getStatusCode() == HttpStatus.OK)){
+            throw  new ServiceNotificationException("Serviço de notificação fora do ar");
+        }
+    }
+}
