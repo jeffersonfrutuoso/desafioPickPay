a12255de1c7ca3874894ce6b833ea080bbc62240
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-21 14:29:06 -0300

    feat: adiciona os endpoints de criar usuario e listar todos os usuarios

---
+++ src/main/java/com/PicPayDesafio/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.PicPayDesafio.controller;
+
+import com.PicPayDesafio.domain.user.User;
+import com.PicPayDesafio.dtos.UserDTO;
+import com.PicPayDesafio.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser( @RequestBody UserDTO userDTO){
+        User newUser = userService.createUser(userDTO);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
