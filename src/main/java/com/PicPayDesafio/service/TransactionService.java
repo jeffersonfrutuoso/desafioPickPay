b9da99f086f978790e77c698326c2a15dd09eec4
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-21 14:26:19 -0300

    feat: adiciona o retorno de uma nova transferencia

--- src/main/java/com/PicPayDesafio/service/TransactionService.java
+++ src/main/java/com/PicPayDesafio/service/TransactionService.java
@@ -15,17 +15,19 @@ public class TransactionService {
 
     private final UserService userService;
     private final AutorizacaoService autorizacaoService;
+    private final NotificationService notificationService;
 
     private final TransactionRepository transactionRepository;
 
-    public TransactionService(UserService userService, AutorizacaoService autorizacaoService, TransactionRepository transactionRepository) {
+    public TransactionService(UserService userService, AutorizacaoService autorizacaoService, NotificationService notificationService, TransactionRepository transactionRepository) {
         this.userService = userService;
         this.autorizacaoService = autorizacaoService;
+        this.notificationService = notificationService;
         this.transactionRepository = transactionRepository;
     }
 
     @Transactional
-    public void createTransaction(TransactionDTO transactionDTO){
+    public Transaction createTransaction(TransactionDTO transactionDTO){
         User enviador = userService.findUserById(transactionDTO.enviadorId());
         User recebedor = userService.findUserById(transactionDTO.recebedorId());
 
@@ -47,6 +49,11 @@ public class TransactionService {
         transactionRepository.save(newTransaction);
         userService.saveUser(enviador);
         userService.saveUser(recebedor);
+
+        notificationService.enviarNotificacao(enviador, "Transferencia realizada com sucesso");
+        notificationService.enviarNotificacao(recebedor, "Transferencia recebida com sucesso");
+
+        return newTransaction;
     }
 
 }
