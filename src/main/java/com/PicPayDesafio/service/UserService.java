d46e3ec36dd2458d7444bd41cc0741eb92fbe1b8
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-21 14:27:32 -0300

    feat: adiciona os endpoints de criar usuario e de listar todos os usuarios

--- src/main/java/com/PicPayDesafio/service/UserService.java
+++ src/main/java/com/PicPayDesafio/service/UserService.java
@@ -1,13 +1,16 @@
 package com.PicPayDesafio.service;
 import com.PicPayDesafio.domain.user.User;
 import com.PicPayDesafio.domain.user.UserType;
+import com.PicPayDesafio.dtos.UserDTO;
 import com.PicPayDesafio.excetion.InsufficientBalanceException;
 import com.PicPayDesafio.excetion.UnauthorizedTransactionException;
 import com.PicPayDesafio.excetion.UserNotFoundException;
+import com.PicPayDesafio.excetion.userAlreadyRegisteredException;
 import com.PicPayDesafio.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -28,8 +31,26 @@ public class UserService {
         }
     }
 
+    public List<User> getAllUsers(){
+      return userRepository.findAll();
+    }
+
     public User findUserById(Long id){
         return userRepository.findUserById(id).orElseThrow(() -> new UserNotFoundException("Usuario nao encontrado"));
+    }
+
+    public User createUser(UserDTO userDTO){
+
+        if (userRepository.existsByDocumento(userDTO.documento())){
+            throw new userAlreadyRegisteredException("Ja existe um usuario com esse documento");
+        }
+
+        if (userRepository.existsByEmail(userDTO.email())){
+            throw new userAlreadyRegisteredException("ja existe um usuario com esse email");
+        }
+        User newUser = new User(userDTO);
+        saveUser(newUser);
+        return newUser;
     }
 
     public void saveUser(User user){
