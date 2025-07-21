e15ecf1a0fe6c1a2fe5f395fb9428b82e6455d10
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-21 14:26:39 -0300

    feat: adiciona o DTO de usuario

--- src/main/java/com/PicPayDesafio/domain/user/User.java
+++ src/main/java/com/PicPayDesafio/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.PicPayDesafio.domain.user;
 
+import com.PicPayDesafio.dtos.UserDTO;
 import jakarta.persistence.*;
 
 import java.math.BigDecimal;
@@ -37,6 +38,15 @@ public class User {
         this.senha = senha;
         this.carteira = carteira;
         this.userType = userType;
+    }
+
+    public User(UserDTO userDTO){
+        this.nome = userDTO.nome();
+        this.email = userDTO.email();
+        this.documento = userDTO.documento();
+        this.senha = userDTO.senha();
+        this.carteira = userDTO.carteira();
+        this.userType = userDTO.userType();
     }
 
     public Long getId() {
