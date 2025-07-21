02dcc898a84381e87a5c892b0fe0ee2dc5b88fe3
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-21 14:27:04 -0300

    feat: adiciona os metodos para saber se o email e o documento ja existem

--- src/main/java/com/PicPayDesafio/repository/UserRepository.java
+++ src/main/java/com/PicPayDesafio/repository/UserRepository.java
@@ -9,4 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findUserById(Long id);
+    boolean existsByEmail(String email);
+    boolean existsByDocumento(String documento);
 }
